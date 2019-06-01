c832f37a6327fc90926a412600d2ccac7bbe2223
ravinath <ravibar1@jaganathan.co.uk>
2019-06-01 16:57:44 +0100

    Add book

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book addBookService(Book book){
+        return bookRepository.save(book);
+    }
+}
